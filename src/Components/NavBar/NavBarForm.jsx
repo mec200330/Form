7ab3efa42f8a06dbeb4b34f2195ab91854f54f73
Cristhian <cristhiangomezdev@gmail.com>
2023-03-19 17:37:26 +0100

    NavBar

---
+++ src/Components/NavBar/NavBarForm.jsx
@@ -0,0 +1,26 @@
+import { Container, Navbar } from "react-bootstrap"
+
+
+const NavBarForm = () => {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+<Container>
+<Navbar.Brand href="#!">
+  Dynamic <b>Form</b>
+  </Navbar.Brand> 
+  <Navbar.Toggle/>
+  <Navbar.Collapse className="justify-content-end">
+    <Navbar.Text>
+      Signed in as:{' '}
+      <a href="https://www.cgomezdev.com" target="_blank" rel="noreferrer">Cristhian Gomez</a>
+    </Navbar.Text>
+  </Navbar.Collapse>
+</Container>
+</Navbar>
+    </div>
+  )
+}
+
+export default NavBarForm
+
