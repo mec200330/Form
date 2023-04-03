ef82b56da07e328f5f4dfd48c0c9b6dff8b48d63
Cristhian <cristhiangomezdev@gmail.com>
2023-04-03 18:48:49 +0200

    tercer paso

--- src/Components/DynamicForm/SecondStep/SecondStep.jsx
+++ src/Components/DynamicForm/SecondStep/SecondStep.jsx
@@ -1,9 +1,52 @@
-import './SecondStep.css'
+import { useState } from 'react';
+import { Col, Container, Form, Row } from 'react-bootstrap';
+import Select from 'react-select';
+import './SecondStep.css';
+
+const options = [
+
+  {id: 1, value:"fútbol", label: "Fútbol ⚽"},
+  {id: 2, value:"PC", label: "PC 💻"},
+  {id: 3, value:"Play", label: "Play 🎮"},
+  {id: 4, value:"Música", label: "Música 🎶"},
+  {id: 5, value:"Foto", label: "Fotografía 📸"},
+  {id: 6, value:"Nadar", label: "Nadar 🏊‍♂️"},
+  {id: 7, value:"Correr", label: "Correr 🏃‍♂️"},
+
+]
+
 
 const SecondStep = () => {
+  const [selectOption, setSelectOption] = useState(null)
+const hobbiesSer = JSON.stringify(selectOption)
+localStorage.setItem('hobbies', hobbiesSer)
+
+  const onChangeAdditionalHobbie =(e)=>{
+    const sport = e.target.value;
+   localStorage.setItem('adittionalHobbie', sport)
+
+  }
   return (
     <div>
-      Segundo paso de prueba
+     <h2>Selecciona tu deporte favorito</h2>
+     <h4>Hobbies</h4>
+     <Container className='p-5 text-center'>
+      <Row className='justify-content-md-center'>
+        <Col lg='7'>
+          <h5>Selecciona tus hobbies</h5>
+          <Select
+          defaultValue={selectOption}
+          onChange={setSelectOption}
+          options={options}
+          isSearchable
+          isMulti/>
+        </Col>
+        <Col lg='7' className='mt-4'>
+          <h5>Quieres añadir otro hobbie??</h5>
+          <Form.Control type='text' placeholder='Karate' onChange={(e)=> onChangeAdditionalHobbie(e)}/>
+        </Col>
+      </Row>
+     </Container>
     </div>
   )
 }
--- src/Components/DynamicForm/ThirdStep/ThirdStep.jsx
+++ src/Components/DynamicForm/ThirdStep/ThirdStep.jsx
@@ -1,9 +1,25 @@
+import { Container, FloatingLabel, Form, Row, Col } from 'react-bootstrap'
 import './ThirdStep.css'
 
 const ThirdStep = () => {
   return (
     <div>
-      Tercer paso de prueba
+     <h2>Tercer paso</h2>
+     <h4>Quieres añadir un comentario??</h4>
+     <Container className='p-5 text-center'>
+      <Row className='justify-content-md-center'>
+        <Col lg="10">
+        <FloatingLabel controlId='floatingTextarea' label="Comentarios" className='mb-3'>
+          <Form.Control as="textarea" placeholder='Enviar y Comentar' onChange={(e) => console.log(e.target.value)}/>
+        </FloatingLabel>
+        </Col>
+      </Row>
+      <div className='send-form'>
+        <p className='result p-3' onClick={()=>console.log("Ver Resultado")}>
+          Ver Resultado 📃
+        </p>
+      </div>
+     </Container>
     </div>
   )
 }
